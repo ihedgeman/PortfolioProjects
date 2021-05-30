0b80257404107760d17b1093d2d8e807de3b8dd4
Imani Hedgeman <54723675+ihedgeman@users.noreply.github.com>
2021-05-29 20:12:54 -0400

    Add files via upload
    Created in MSsql Server

---
+++ Covid-19 Project Code for Tableau.sql
@@ -0,0 +1,57 @@
+/*
+Queries used for Tableau Project
+*/
+
+
+
+-- 1. 
+
+SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS INT)) AS total_deaths, SUM(CAST(new_deaths AS INT))/SUM(New_Cases)*100 AS DeathPercentage
+From CovidProject..CovidDeaths
+--Where location like '%states%'
+WHERE continent IS NOT NULL		 
+--Group By date
+ORDER BY 1,2;
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+--SELECT SUM(new_cases) as total_cases, SUM(CAST(new_deaths AS INT)) as total_deaths, SUM(CAST(new_deaths AS INT))/SUM(New_Cases)*100 as DeathPercentage
+--From CovidProject..CovidDeaths
+----Where location like '%states%'
+--where location = 'World'
+----Group By date
+--order by 1,2
+
+
+-- 2. 
+
+-- European Union is part of Europe
+
+SELECT location, SUM(CAST(new_deaths AS INT)) AS TotalDeathCount
+From CovidProject..CovidDeaths
+--Where location like '%states%'
+WHERE continent IS NULL 
+AND location NOT IN ('World', 'European Union', 'International')
+GROUP BY location
+ORDER BY TotalDeathCount DESC;
+
+
+-- 3.
+
+SELECT Location, Population, MAX(total_cases) AS HighestInfectionCount,  MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM CovidProject..CovidDeaths
+--Where location like '%states%'
+GROUP BY Location, Population
+ORDER BY PercentPopulationInfected DESC;
+
+
+-- 4.
+
+
+SELECT Location, Population,date, MAX(total_cases) AS HighestInfectionCount,  MAX((total_cases/population))*100 AS PercentPopulationInfected
+FROM CovidProject..CovidDeaths
+--Where location like '%states%'
+GROUP BY Location, Population, date
+ORDER BY PercentPopulationInfected DESC;
